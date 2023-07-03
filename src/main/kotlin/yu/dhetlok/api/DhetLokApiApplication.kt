f6f11f0acf585c651c6e8eb554fe67aea2d09d70
NBDatsuya <2646809050@qq.com>
2023-07-03 16:30:53 +0800

    Initial commit

---
+++ src/main/kotlin/yu/dhetlok/api/DhetLokApiApplication.kt
@@ -0,0 +1,11 @@
+package yu.dhetlok.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DhetLokApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DhetLokApiApplication>(*args)
+}
---
+++ src/test/kotlin/yu/dhetlok/api/DhetLokApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package yu.dhetlok.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DhetLokApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
